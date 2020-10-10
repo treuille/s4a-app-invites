863aa950f033b4309b01a50220fe8508f563044a
Adrien Treuille <adrien.g.treuille@gmail.com>
2020-10-10 10:04:22 -0700

    Now loading a cached github access token.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,27 @@
 import streamlit as st
 import zipfile
 import pandas as pd
+from github import Github
+from github import MainClass as GithubMainClass
+import hashlib
 
 "# S4A App Explorer"
+
+# Utilities to hash the github class properly.
+
+def hash_github_object(github):
+    """Hashes the github object so that we can reuse it."""
+    return github._access_token
+
+MY_HASH_FUNCS = {GithubMainClass.Github: hash_github_object}
+
+@st.cache(hash_funcs=MY_HASH_FUNCS)
+def github_object_from_access_token(access_token):
+    github = Github(access_token)
+    github._access_token = access_token
+    return github
+
+# Parsing the user's input
 
 def err(msg):
     """Print an error message to the user and stop."""
@@ -11,11 +30,22 @@ def err(msg):
 
 def get_config():
     """Returns all the config information to run the app."""
+    
+    # Get input
     config = st.sidebar.beta_expander('Config', expanded=True)
     raw_input = config.file_uploader('User list zip file.', types='zip')
+    access_token = config.text_input("Github access token", type="password")
+
+    # Check for input
     if not raw_input:
         err('Please upload a user file. Ask TC for the file.')
-    return raw_input
+    if not access_token:
+        err('Please enter a github access token.')
+
+    # Parse and return the information
+    user_table = extract_csv_from_zip_file(raw_input)
+    github = github_object_from_access_token(access_token)
+    return user_table, github
 
 @st.cache
 def extract_csv_from_zip_file(raw_input):
@@ -26,8 +56,13 @@ def extract_csv_from_zip_file(raw_input):
         with zip_input.open(names[0]) as csv_input:
             return pd.read_csv(csv_input)
 
-raw_input = get_config()
-user_table = extract_csv_from_zip_file(raw_input)
+
+user_table, github = get_config()
+'github object', type(github), dir(github)
+st.write(github.oauth_scopes)
+st.stop()
+
+
 status = st.selectbox('Status', list(set(user_table['Status'])))
 filtered_users = user_table[user_table['Status'] == status]
 filtered_users = filtered_users.sort_values(by='Applied At', ascending=True)
@@ -37,3 +72,4 @@ for user in filtered_users.itertuples():
     # st.text(user)
     # st.text(type(user))
     break
+
