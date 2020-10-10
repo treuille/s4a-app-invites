6cf7d145b2958740e624e24bcfa59fd6dfa25523
Adrien Treuille <adrien.g.treuille@gmail.com>
2020-10-10 10:33:59 -0700

    Now loading and caching github usernames

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,17 @@ def github_object_from_access_token(access_token):
     github._access_token = access_token
     return github
 
+@st.cache(hash_funcs=MY_HASH_FUNCS)
+def get_user_from_email(github, email):
+    """Returns a user for that email or None."""
+    users = list(github.search_users(f'type:user {email} in:email'))
+    if len(users) == 0:
+        return None
+    elif len(users) == 1:
+        return users[0]
+    else:
+        raise RuntimeError(f'{email} associated with {len(users)} users.')
+
 # Parsing the user's input
 
 def err(msg):
@@ -58,18 +69,19 @@ def extract_csv_from_zip_file(raw_input):
 
 
 user_table, github = get_config()
-'github object', type(github), dir(github)
-st.write(github.oauth_scopes)
-st.stop()
-
-
 status = st.selectbox('Status', list(set(user_table['Status'])))
 filtered_users = user_table[user_table['Status'] == status]
 filtered_users = filtered_users.sort_values(by='Applied At', ascending=True)
 filtered_users, len(filtered_users)
-for user in filtered_users.itertuples():
-    st.write(dir(user))
-    # st.text(user)
-    # st.text(type(user))
-    break
+for s4a_user in filtered_users.itertuples():
+    f'email: `{s4a_user.Email}`'
+    github_user = get_user_from_email(github, s4a_user.Email)
+    if not github_user:
+        st.warning('Could not find ' + s4a_user.Email)
+        continue
+    f'twitter_username: `{github_user.twitter_username}`'
+    f'created_at: `{github_user.created_at}`'
+    f'name: `{github_user.name}`'
+    f'login: `{github_user.login}`'
+    raise RuntimeError('Stopping here.')
 
